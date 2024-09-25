83b7cc3db0353148fb7197f40d5a04346a39c71c
hpc0326 <jack20010326@gmail.com>
2024-09-25 03:54:11 -0700

    fix:pgadmin config

--- script/data_visualization.sh
+++ script/data_visualization.sh
@@ -5,6 +5,34 @@ echo "===================="
 echo "build .env file for pgadmin"
 echo "===================="
 cp .env.pgadmin_config ./data_visualization/pgadmin/.env
+source .env.postgres_config
+mkdir -p ./data_visualization/pgadmin-server_config
+cat <<EOR > ./data_visualization/pgadmin-server_config/servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "${METADATA_POSTGRES_DB}",
+            "Group": "Servers",
+            "Host": "${DEPLOYMENT_PF_HOST_IP}",
+            "Port": "${METADATA_POSTGRES_CONTAINER_PORT}",
+            "MaintenanceDB": "${METADATA_POSTGRES_DB}",
+            "Username": "${METADATA_POSTGRES_USER}",
+            "Password": "${METADATA_POSTGRES_PASSWORD}",
+            "SSLMode": "prefer"
+        },
+        "2": {
+            "Name": "${AUTHENTICATE_POSTGRES_DB}",
+            "Group": "Servers",
+            "Host": "${DEPLOYMENT_PF_HOST_IP}",
+            "Port": "${AUTHENTICATE_POSTGRES_CONTAINER_PORT}",
+            "MaintenanceDB": "${AUTHENTICATE_POSTGRES_DB}",
+            "Username": "${AUTHENTICATE_POSTGRES_USER}",
+            "Password": "${AUTHENTICATE_POSTGRES_PASSWORD}",
+            "SSLMode": "prefer"
+        }
+    }
+}
+EOR
 
 echo "===================="
 echo "build container for pgadmin"
